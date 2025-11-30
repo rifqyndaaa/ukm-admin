213710eff17bf1bf613fe29ffe7fd339c2160654
rifqyndaaa <yanda24si@mahasiswa.pcr.ac.id>
2025-11-30 20:56:47 +0700

    perbaiki 1

--- app/Models/umkm.php
+++ app/Models/umkm.php
@@ -9,19 +9,12 @@ class Umkm extends Model
 {
     use HasFactory;
 
-    // Nama tabel yang digunakan
     protected $table = 'umkm';
-
-    // Primary key custom (karena bukan 'id')
     protected $primaryKey = 'umkm_id';
-
-    // Jika primary key bukan tipe UUID
     public $incrementing = true;
-
-    // Tipe data dari primary key
     protected $keyType = 'int';
+    public $timestamps = true;
 
-    // Kolom yang bisa diisi secara massal (fillable)
     protected $fillable = [
         'nama_usaha',
         'pemilik_warga_id',
@@ -31,21 +24,16 @@ class Umkm extends Model
         'kategori',
         'kontak',
         'deskripsi',
-        'foto_usaha'
+        'foto_usaha',
     ];
 
-    // Jika kamu pakai timestamps (created_at & updated_at)
-    public $timestamps = true;
+    public function warga()
+    {
+        return $this->belongsTo(Warga::class, 'pemilik_warga_id', 'warga_id');
+    }
 
-     // Relasi ke tabel lain (opsional, aktifkan kalau sudah ada)
-     public function warga()
-     {
-       return $this->belongsTo(Warga::class, 'pemilik_warga_id', 'warga_id');
-     }
-
-     public function produk()
-     {
+    public function produk()
+    {
         return $this->hasMany(Produk::class, 'umkm_id', 'umkm_id');
-     }
+    }
 }
-
