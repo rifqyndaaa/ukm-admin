295ed4948d096c7eff52c3bfd14d12e65393995e
rifqyndaaa <yanda24si@mahasiswa.pcr.ac.id>
2025-11-30 21:13:17 +0700

    tes3

--- app/Http/Controllers/WargaController.php
+++ app/Http/Controllers/WargaController.php
@@ -7,15 +7,42 @@ use Illuminate\Http\Request;
 
 class WargaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $warga = Warga::all();
-        return view('pages.warga.index', compact('warga'));
+        $query = Warga::query();
+
+        // SEARCH (nama warga)
+        if ($request->filled('search')) {
+            $keyword = $request->search;
+            $query->where('nama', 'LIKE', "%$keyword%");
+        }
+
+        // FILTER JENIS KELAMIN
+        if ($request->filled('jenis_kelamin')) {
+            $query->where('jenis_kelamin', $request->jenis_kelamin);
+        }
+
+        // FILTER AGAMA
+        if ($request->filled('agama')) {
+            $query->where('agama', $request->agama);
+        }
+
+        // PAGINATION
+        $warga = $query->paginate(10)->withQueryString();
+
+        // Data dropdown untuk filter
+        $listJenisKelamin = ['Laki-laki', 'Perempuan'];
+        $listAgama = ['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu'];
+
+        return view('pages.warga.index', compact('warga', 'listJenisKelamin', 'listAgama'));
     }
 
     public function create()
     {
-        return view('pages.warga.create');
+        $listJenisKelamin = ['Laki-laki', 'Perempuan'];
+        $listAgama = ['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu'];
+
+        return view('pages.warga.create', compact('listJenisKelamin', 'listAgama'));
     }
 
     public function store(Request $request)
@@ -23,12 +50,47 @@ class WargaController extends Controller
         $request->validate([
             'no_ktp' => 'required|unique:warga,no_ktp',
             'nama' => 'required',
-            'jenis_kelamin' => 'required',
+            'jenis_kelamin' => 'required|in:Laki-laki,Perempuan',
             'agama' => 'required',
+            'alamat' => 'nullable|string',
         ]);
 
         Warga::create($request->all());
 
         return redirect()->route('warga.index')->with('success', 'Data berhasil ditambahkan.');
     }
+
+    public function edit($id)
+    {
+        $warga = Warga::findOrFail($id);
+        $listJenisKelamin = ['Laki-laki', 'Perempuan'];
+        $listAgama = ['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu'];
+
+        return view('pages.warga.edit', compact('warga', 'listJenisKelamin', 'listAgama'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'no_ktp' => 'required|unique:warga,no_ktp,' . $id . ',warga_id',
+            'nama' => 'required',
+            'jenis_kelamin' => 'required|in:Laki-laki,Perempuan',
+            'agama' => 'required',
+            'alamat' => 'nullable|string',
+        ]);
+
+        $warga = Warga::findOrFail($id);
+        $warga->update($request->all());
+
+        return redirect()->route('warga.index')->with('success', 'Data berhasil diperbarui.');
+    }
+
+    public function destroy($id)
+    {
+        $warga = Warga::findOrFail($id);
+        $warga->delete();
+
+        return redirect()->route('warga.index')->with('success', 'Data berhasil dihapus.');
+    }
 }
+
